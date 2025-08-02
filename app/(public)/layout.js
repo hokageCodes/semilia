b7f5c7e78c18705b6177b24519ea2f8cff15216c
hokageCodes <hashsenju94@gmail.com>
2025-08-02 18:16:39 +0100

    navbar and foter and auth for navbar workign

--- app/(public)/layout.js
+++ app/(public)/layout.js
@@ -1,11 +1,13 @@
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
+import { ToastContainer } from "react-toastify";
 
 export default function PublicLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1">{children}</main>
+      <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar />
       <Footer />
     </div>
   );
